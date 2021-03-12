2c011a89c6446fd9928896f3efc6d9b04b378972
Juice <giuseppe.modarelli@gmail.com>
2021-03-12 18:58:28 +0100

    Create a scene renderer

--- src/main.zig
+++ src/main.zig
@@ -1,51 +1,14 @@
 const std = @import("std");
-const builtin = @import("builtin");
 const panic = std.debug.panic;
-
 const c = @import("c.zig");
-const shaders = @import("shaders.zig");
-
-const math = @import("math.zig");
-const Vec3 = math.Vec3;
-const Mat4 = math.Mat4;
-
-const model = @import("model.zig");
-const PngImage = @import("textures.zig").PngImage;
+const scene_renderer = @import("scene_renderer.zig");
+const Scene = scene_renderer.Scene;
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 var global_allocator = &gpa.allocator;
 
 const SCR_WIDTH: u32 = 1920;
 const SCR_HEIGHT: u32 = 1080;
-
-const SceneParams = struct {
-    view_matrix: Mat4(f32),
-    proj_matrix: Mat4(f32),
-};
-
-const ModelTransform = struct {
-    model_matrix: Mat4(f32),
-};
-
-const Material = struct {
-    albedo_texture_idx: usize,
-};
-
-const Node = struct {
-    mesh_idx: usize,
-    material_idx: usize,
-    position: Vec3(f32),
-    rotation: Vec3(f32),
-    scale: Vec3(f32),
-    transform: ModelTransform,
-};
-
-const Scene = struct {
-    meshes: []model.Mesh,
-    textures: []c.GLuint,
-    materials: []Material,
-    nodes: []Node,
-};
 
 pub fn main() !void {
     const ok = c.glfwInit();
@@ -60,7 +23,7 @@ pub fn main() !void {
     c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GL_TRUE);
     c.glfwWindowHint(c.GLFW_OPENGL_DEBUG_CONTEXT, c.GL_TRUE);
 
-    var window = c.glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Learn Zig", null, null);
+    var window = c.glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Zigl", null, null);
     if (window == null) {
         panic("Failed to create GLFW window\n", .{});
     }
@@ -85,152 +48,8 @@ pub fn main() !void {
     c.glDebugMessageCallback(opengl_debug_callback, null);
     c.glEnable(c.GL_DEBUG_OUTPUT_SYNCHRONOUS);
 
-    // Load "level/scene" simulation
-    // The idea here is to have a simple (flat) scene file format
-    // which enumerates the elements (meshes, textures, materials, lights, etc...)
-    // present in the scene in a way that we can load them in memory without
-    // the need for dynamic allocations (like ArrayList)
-    //
-    // Scene data container
-    // --------------------
     var scene: Scene = undefined;
-    const mesh_count = 2; // TODO: This will be read for a scene file
-    scene.meshes = try global_allocator.alloc(model.Mesh, mesh_count);
-    // Load all meshes
-    // ---------------
-    var geometry: model.Geometry = undefined;
-    geometry.init(global_allocator);
-    // TODO: We will iterate over all the meses in the scene file
-    {
-        scene.meshes[0] = try geometry.loadObj("data/models/default_cube.obj");
-        scene.meshes[1] = try geometry.loadObj("data/models/suzanne.obj");
-    }
-    // Load Textures
-    // -------------
-    const texture_count = 1; // TODO: This will be read for a scene file
-    scene.textures = try global_allocator.alloc(c.GLuint, texture_count);
-    // TODO: We will iterate over all the textures in the scene file
-    {
-        const file = try std.fs.cwd().openFile("data/textures/uvgrid.png", .{});
-        var data: []u8 = try file.readToEndAlloc(global_allocator, 1024 * 1024);
-        file.close();
-        var pi = try PngImage.create(data);
-
-        c.glCreateTextures(c.GL_TEXTURE_2D, 1, &scene.textures[0]);
-        c.glTextureStorage2D(scene.textures[0], 1, c.GL_RGBA8, @intCast(c_int, pi.width), @intCast(c_int, pi.height));
-        c.glTextureSubImage2D(scene.textures[0], 0, 0, 0, @intCast(c_int, pi.width), @intCast(c_int, pi.height), c.GL_RGBA, c.GL_UNSIGNED_BYTE, @ptrCast(*c_void, &pi.raw[0]));
-
-        global_allocator.free(data);
-        PngImage.destroy(&pi);
-    }
-    // Load Materials
-    // --------------
-    const material_count = 2; // TODO: This will be read for a scene file
-    scene.materials = try global_allocator.alloc(Material, material_count);
-    // TODO: We will iterate over all the materials in the scene file
-    {
-        scene.materials[0] = Material{
-            .albedo_texture_idx = 0,
-        };
-        scene.materials[1] = Material{
-            .albedo_texture_idx = 0,
-        };
-    }
-    // Load Nodes
-    // ----------
-    const node_count = 2; // TODO: This will be read for a scene file
-    scene.nodes = try global_allocator.alloc(Node, node_count);
-    // TODO: We will iterate over all the nodesl in the scene file
-    {
-        scene.nodes[0] = Node{
-            .mesh_idx = 0,
-            .material_idx = 0,
-            .position = Vec3(f32).init(1.5, 0, -4),
-            .rotation = Vec3(f32).init(0, 0, 0),
-            .scale = Vec3(f32).init(1, 1, 1),
-            .transform = undefined,
-        };
-        scene.nodes[0].transform = ModelTransform{
-            .model_matrix = Mat4(f32).TRS(scene.nodes[0].position, scene.nodes[0].rotation, scene.nodes[0].scale),
-        };
-
-        scene.nodes[1] = Node{
-            .mesh_idx = 1,
-            .material_idx = 1,
-            .position = Vec3(f32).init(-1.5, 0, -4),
-            .rotation = Vec3(f32).init(0, 0, 0),
-            .scale = Vec3(f32).init(1, 1, 1),
-            .transform = undefined,
-        };
-        scene.nodes[1].transform = ModelTransform{
-            .model_matrix = Mat4(f32).TRS(scene.nodes[1].position, scene.nodes[1].rotation, scene.nodes[1].scale),
-        };
-    }
-
-    // Load scene settings
-    var scene_params = SceneParams{
-        .view_matrix = Mat4(f32).lookAt(Vec3(f32).init(0, 0, 0), Vec3(f32).init(0, 0, -1), Vec3(f32).init(0, 1, 0)),
-        .proj_matrix = Mat4(f32).perspective(60.0, @intToFloat(f32, SCR_WIDTH) / @intToFloat(f32, SCR_HEIGHT), 0.001, 1000.0),
-    };
-
-    var scene_uniform_buffer: c.GLuint = undefined;
-    c.glCreateBuffers(1, &scene_uniform_buffer);
-    c.glNamedBufferStorage(scene_uniform_buffer, @intCast(c_longlong, @sizeOf(SceneParams)), &scene_params, c.GL_DYNAMIC_STORAGE_BIT);
-
-    var node_uniform_buffer: c.GLuint = undefined;
-    c.glCreateBuffers(1, &node_uniform_buffer);
-    c.glNamedBufferStorage(node_uniform_buffer, @intCast(c_longlong, @sizeOf(ModelTransform)), null, c.GL_DYNAMIC_STORAGE_BIT);
-
-    var vao: c.GLuint = undefined;
-    var vbo: c.GLuint = undefined;
-    var ebo: c.GLuint = undefined;
-
-    // Create the Vertex Array Object
-    c.glGenVertexArrays(1, &vao);
-    c.glBindVertexArray(vao);
-
-    // Upload all geometry to a vertex and index buffer on the GPU
-    {
-        // Allocate and initialize a vertex buffer object
-        c.glCreateBuffers(1, &vbo);
-        c.glNamedBufferStorage(vbo, @intCast(c_longlong, @sizeOf(model.Vertex) * geometry.vertices.items.len), geometry.vertices.items.ptr, 0);
-
-        // Allocate and initialize an index buffer object
-        c.glCreateBuffers(1, &ebo);
-        c.glNamedBufferStorage(ebo, @intCast(c_longlong, @sizeOf(u32) * geometry.indices.items.len), geometry.indices.items.ptr, 0);
-
-        // We no longer need a copy of the data on the CPU
-        geometry.deinit();
-    }
-
-    // Bind the buffer to the vertex array object
-    c.glVertexArrayVertexBuffer(vao, 0, vbo, 0, @sizeOf(model.Vertex));
-
-    // Set up two vertex attributes.
-    // Position
-    c.glVertexArrayAttribBinding(vao, 0, 0);
-    c.glVertexArrayAttribFormat(vao, 0, 3, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "position"));
-    c.glEnableVertexAttribArray(0);
-    // Normal
-    c.glVertexArrayAttribBinding(vao, 1, 0);
-    c.glVertexArrayAttribFormat(vao, 1, 3, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "normal"));
-    c.glEnableVertexAttribArray(1);
-    // UV
-    c.glVertexArrayAttribBinding(vao, 2, 0);
-    c.glVertexArrayAttribFormat(vao, 2, 2, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "uv0"));
-    c.glEnableVertexAttribArray(2);
-
-    c.glBindVertexArray(0);
-
-    const shader_program = try shaders.createProgram(global_allocator, "data/shaders/uber_vertex.glsl", "data/shaders/uber_fragment.glsl");
-
-    // Create samplers for the program
-    var albedo_sampler: c.GLuint = undefined;
-    c.glCreateSamplers(1, &albedo_sampler);
-    c.glSamplerParameteri(albedo_sampler, c.GL_TEXTURE_WRAP_S, c.GL_CLAMP_TO_EDGE);
-    c.glSamplerParameteri(albedo_sampler, c.GL_TEXTURE_WRAP_T, c.GL_CLAMP_TO_EDGE);
-    c.glSamplerParameteri(albedo_sampler, c.GL_TEXTURE_MIN_FILTER, c.GL_LINEAR_MIPMAP_LINEAR);
-    c.glSamplerParameteri(albedo_sampler, c.GL_TEXTURE_MAG_FILTER, c.GL_LINEAR);
+    try scene.init(global_allocator, SCR_WIDTH, SCR_HEIGHT);
 
     var current_time = c.glfwGetTime();
     var last_time = current_time;
@@ -243,30 +62,7 @@ pub fn main() !void {
         c.glClearBufferfv(c.GL_COLOR, 0, @ptrCast([*c]const c.GLfloat, &color));
         c.glClearBufferfi(c.GL_DEPTH_STENCIL, 0, 1.0, 0);
 
-        // Bind vertex buffer
-        c.glBindVertexArray(vao);
-
-        c.glUseProgram(shader_program);
-        c.glFrontFace(c.GL_CCW);
-        c.glEnable(c.GL_CULL_FACE);
-        c.glEnable(c.GL_DEPTH_TEST);
-        c.glDepthFunc(c.GL_LEQUAL);
-
-        // Binding program samplers
-        c.glBindSampler(0, albedo_sampler);
-
-        c.glBindBufferBase(c.GL_UNIFORM_BUFFER, 0, scene_uniform_buffer);
-
-        c.glBindBuffer(c.GL_ELEMENT_ARRAY_BUFFER, ebo);
-
-        for (scene.nodes) |node| {
-            c.glNamedBufferSubData(node_uniform_buffer, 0, @intCast(c_longlong, @sizeOf(ModelTransform)), &node.transform);
-            c.glBindBufferBase(c.GL_UNIFORM_BUFFER, 1, node_uniform_buffer);
-            c.glBindTextureUnit(0, scene.textures[scene.materials[node.material_idx].albedo_texture_idx]);
-            c.glDrawElementsBaseVertex(c.GL_TRIANGLES, @intCast(c_int, scene.meshes[node.mesh_idx].index_count), c.GL_UNSIGNED_INT, @intToPtr(?*const c_void, scene.meshes[node.mesh_idx].index_offset), @intCast(c.GLint, scene.meshes[node.mesh_idx].vertex_base));
-        }
-
-        c.glBindVertexArray(0);
+        scene.render();
 
         c.glfwSwapBuffers(window);
         c.glfwPollEvents();
@@ -276,10 +72,7 @@ pub fn main() !void {
         last_time = current_time;
     }
 
-    global_allocator.free(scene.meshes);
-    global_allocator.free(scene.textures);
-    global_allocator.free(scene.materials);
-    global_allocator.free(scene.nodes);
+    scene.deinit();
 
     const leaked = gpa.deinit();
     if (leaked) {
---
+++ src/scene_renderer.zig
@@ -0,0 +1,231 @@
+const std = @import("std");
+const c = @import("c.zig");
+const model = @import("model.zig");
+const math = @import("math.zig");
+const shaders = @import("shaders.zig");
+const PngImage = @import("textures.zig").PngImage;
+const Mat4f = math.Mat4(f32);
+const Vec3f = math.Vec3(f32);
+
+pub const SceneParams = struct {
+    view_matrix: Mat4f,
+    proj_matrix: Mat4f,
+};
+
+pub const ModelTransform = struct {
+    model_matrix: Mat4f,
+};
+
+pub const Material = struct {
+    albedo_texture_idx: usize,
+};
+
+pub const Node = struct {
+    mesh_idx: usize,
+    material_idx: usize,
+    position: Vec3f,
+    rotation: Vec3f,
+    scale: Vec3f,
+    transform: ModelTransform,
+};
+
+pub const Scene = struct {
+    scene_params: SceneParams,
+
+    vao: c.GLuint,
+    vbo: c.GLuint,
+    ebo: c.GLuint,
+
+    scene_uniform_buffer: c.GLuint,
+    node_uniform_buffer: c.GLuint,
+
+    uber_shader: c.GLuint,
+    albedo_sampler: c.GLuint, 
+
+    meshes: []model.Mesh,
+    textures: []c.GLuint,
+    materials: []Material,
+    nodes: []Node,
+
+    allocator: *std.mem.Allocator,
+
+    // TODO: Pass the path to a scene file
+    pub fn init(scene: *Scene, allocator: *std.mem.Allocator, screen_width: u32, screen_height: u32) !void {
+        scene.allocator = allocator;
+
+        // Scene data container
+        // --------------------
+        const mesh_count = 2; // TODO: This will be read for the scene file
+        scene.meshes = try allocator.alloc(model.Mesh, mesh_count);
+        // Load all meshes
+        // ---------------
+        var geometry: model.Geometry = undefined;
+        geometry.init(allocator);
+        // TODO: We will iterate over all the meses in the scene file
+        {
+            scene.meshes[0] = try geometry.loadObj("data/models/default_cube.obj");
+            scene.meshes[1] = try geometry.loadObj("data/models/suzanne.obj");
+        }
+
+        // Load Textures
+        // -------------
+        const texture_count = 1; // TODO: This will be read from the scene file
+        scene.textures = try allocator.alloc(c.GLuint, texture_count);
+        c.glCreateTextures(c.GL_TEXTURE_2D, texture_count, @ptrCast([*c]c_uint, scene.textures.ptr));
+
+        // TODO: We will iterate over all the textures in the scene file
+        {
+            const file = try std.fs.cwd().openFile("data/textures/uvgrid.png", .{});
+            var data: []u8 = try file.readToEndAlloc(allocator, 1024 * 1024);
+            file.close();
+            var pi = try PngImage.create(data);
+
+            c.glTextureStorage2D(scene.textures[0], 1, c.GL_RGBA8, @intCast(c_int, pi.width), @intCast(c_int, pi.height));
+            c.glTextureSubImage2D(scene.textures[0], 0, 0, 0, @intCast(c_int, pi.width), @intCast(c_int, pi.height), c.GL_RGBA, c.GL_UNSIGNED_BYTE, @ptrCast(*c_void, &pi.raw[0]));
+
+            allocator.free(data);
+            PngImage.destroy(&pi);
+        }
+
+        // Load Materials
+        // --------------
+        const material_count = 2; // TODO: This will be read for a scene file
+        scene.materials = try allocator.alloc(Material, material_count);
+        // TODO: We will iterate over all the materials in the scene file
+        {
+            scene.materials[0] = Material{
+                .albedo_texture_idx = 0,
+            };
+            scene.materials[1] = Material{
+                .albedo_texture_idx = 0,
+            };
+        }
+
+        // Load Nodes
+        // ----------
+        const node_count = 2; // TODO: This will be read for a scene file
+        scene.nodes = try allocator.alloc(Node, node_count);
+        // TODO: We will iterate over all the nodesl in the scene file
+        {
+            scene.nodes[0] = Node{
+                .mesh_idx = 0,
+                .material_idx = 0,
+                .position = Vec3f.init(1.5, 0, -4),
+                .rotation = Vec3f.init(0, 0, 0),
+                .scale = Vec3f.init(1, 1, 1),
+                .transform = undefined,
+            };
+            scene.nodes[0].transform = ModelTransform{
+                .model_matrix = Mat4f.TRS(scene.nodes[0].position, scene.nodes[0].rotation, scene.nodes[0].scale),
+            };
+
+            scene.nodes[1] = Node{
+                .mesh_idx = 1,
+                .material_idx = 1,
+                .position = Vec3f.init(-1.5, 0, -4),
+                .rotation = Vec3f.init(0, 0, 0),
+                .scale = Vec3f.init(1, 1, 1),
+                .transform = undefined,
+            };
+            scene.nodes[1].transform = ModelTransform{
+                .model_matrix = Mat4f.TRS(scene.nodes[1].position, scene.nodes[1].rotation, scene.nodes[1].scale),
+            };
+        }
+
+        // Load scene settings
+        scene.scene_params = SceneParams{
+            .view_matrix = Mat4f.lookAt(Vec3f.init(0, 0, 0), Vec3f.init(0, 0, -1), Vec3f.init(0, 1, 0)),
+            .proj_matrix = Mat4f.perspective(60.0, @intToFloat(f32, screen_width) / @intToFloat(f32, screen_height), 0.001, 1000.0),
+        };
+
+        // Create the Vertex Array Object
+        c.glGenVertexArrays(1, &scene.vao);
+        c.glBindVertexArray(scene.vao);
+
+        // Upload all geometry to a vertex and index buffer on the GPU
+        {
+            // Allocate and initialize a vertex buffer object
+            c.glCreateBuffers(1, &scene.vbo);
+            c.glNamedBufferStorage(scene.vbo, @intCast(c_longlong, @sizeOf(model.Vertex) * geometry.vertices.items.len), geometry.vertices.items.ptr, 0);
+
+            // Allocate and initialize an index buffer object
+            c.glCreateBuffers(1, &scene.ebo);
+            c.glNamedBufferStorage(scene.ebo, @intCast(c_longlong, @sizeOf(u32) * geometry.indices.items.len), geometry.indices.items.ptr, 0);
+
+            // We no longer need a copy of the data on the CPU
+            geometry.deinit();
+        }
+
+        // Bind the buffer to the vertex array object
+        c.glVertexArrayVertexBuffer(scene.vao, 0, scene.vbo, 0, @sizeOf(model.Vertex));
+
+        // Set up two vertex attributes.
+        // Position
+        c.glVertexArrayAttribBinding(scene.vao, 0, 0);
+        c.glVertexArrayAttribFormat(scene.vao, 0, 3, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "position"));
+        c.glEnableVertexAttribArray(0);
+        // Normal
+        c.glVertexArrayAttribBinding(scene.vao, 1, 0);
+        c.glVertexArrayAttribFormat(scene.vao, 1, 3, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "normal"));
+        c.glEnableVertexAttribArray(1);
+        // UV
+        c.glVertexArrayAttribBinding(scene.vao, 2, 0);
+        c.glVertexArrayAttribFormat(scene.vao, 2, 2, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "uv0"));
+        c.glEnableVertexAttribArray(2);
+
+        c.glBindVertexArray(0);
+
+        scene.uber_shader = try shaders.createProgram(allocator, "data/shaders/uber_vertex.glsl", "data/shaders/uber_fragment.glsl");
+
+        // Create samplers for the program
+        c.glCreateSamplers(1, &scene.albedo_sampler);
+        c.glSamplerParameteri(scene.albedo_sampler, c.GL_TEXTURE_WRAP_S, c.GL_CLAMP_TO_EDGE);
+        c.glSamplerParameteri(scene.albedo_sampler, c.GL_TEXTURE_WRAP_T, c.GL_CLAMP_TO_EDGE);
+        c.glSamplerParameteri(scene.albedo_sampler, c.GL_TEXTURE_MIN_FILTER, c.GL_LINEAR_MIPMAP_LINEAR);
+        c.glSamplerParameteri(scene.albedo_sampler, c.GL_TEXTURE_MAG_FILTER, c.GL_LINEAR);
+
+        c.glCreateBuffers(1, &scene.scene_uniform_buffer);
+        c.glNamedBufferStorage(scene.scene_uniform_buffer, @intCast(c_longlong, @sizeOf(SceneParams)), &scene.scene_params, c.GL_DYNAMIC_STORAGE_BIT);
+
+        c.glCreateBuffers(1, &scene.node_uniform_buffer);
+        c.glNamedBufferStorage(scene.node_uniform_buffer, @intCast(c_longlong, @sizeOf(ModelTransform)), null, c.GL_DYNAMIC_STORAGE_BIT);
+    }
+
+    pub fn deinit(self: *Scene) void {
+        self.allocator.free(self.meshes);
+        self.allocator.free(self.textures);
+        self.allocator.free(self.materials);
+        self.allocator.free(self.nodes);
+    }
+
+    pub fn update(self: *Scene, delta_time: f32) void {
+
+    }
+
+    pub fn render(self: *Scene) void {
+        c.glBindVertexArray(self.vao);
+
+        c.glUseProgram(self.uber_shader);
+        c.glFrontFace(c.GL_CCW);
+        c.glEnable(c.GL_CULL_FACE);
+        c.glEnable(c.GL_DEPTH_TEST);
+        c.glDepthFunc(c.GL_LEQUAL);
+
+        // Binding program samplers
+        c.glBindSampler(0, self.albedo_sampler);
+
+        c.glBindBufferBase(c.GL_UNIFORM_BUFFER, 0, self.scene_uniform_buffer);
+
+        c.glBindBuffer(c.GL_ELEMENT_ARRAY_BUFFER, self.ebo);
+
+        for (self.nodes) |node| {
+            c.glNamedBufferSubData(self.node_uniform_buffer, 0, @intCast(c_longlong, @sizeOf(ModelTransform)), &node.transform);
+            c.glBindBufferBase(c.GL_UNIFORM_BUFFER, 1, self.node_uniform_buffer);
+            c.glBindTextureUnit(0, self.textures[self.materials[node.material_idx].albedo_texture_idx]);
+            c.glDrawElementsBaseVertex(c.GL_TRIANGLES, @intCast(c_int, self.meshes[node.mesh_idx].index_count), c.GL_UNSIGNED_INT, @intToPtr(?*const c_void, self.meshes[node.mesh_idx].index_offset), @intCast(c.GLint, self.meshes[node.mesh_idx].vertex_base));
+        }
+
+        // NOTE: Do we need to unbind this?
+        c.glBindVertexArray(0);
+    }
+};
