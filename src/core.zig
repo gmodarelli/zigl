4f281c829face41c8866ce65bc744eb2546707b4
Juice <giuseppe.modarelli@gmail.com>
2021-03-13 16:15:03 +0100

    Moving core around

---
+++ src/core.zig
@@ -0,0 +1,175 @@
+const c = @import("c.zig");
+const std = @import("std");
+const panic = std.debug.panic;
+
+const SceneRenderer = @import("scene_renderer.zig").SceneRenderer;
+const CameraMovement = @import("camera.zig").CameraMovement;
+
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+var global_allocator = &gpa.allocator;
+
+pub const App = struct {
+    const Self = @This();
+
+    window: ?*c.GLFWwindow = null,
+    current_time: f64,
+    last_time: f64,
+    delta_time: f32,
+
+    scene: SceneRenderer,
+
+    pub fn init(self: *Self, width: u32, height: u32) !void {
+        if (c.glfwInit() == 0) {
+            panic("Failed to initialize GLFW\n", .{});
+        }
+
+        c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 4);
+        c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 5);
+        c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
+        c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GL_TRUE);
+        c.glfwWindowHint(c.GLFW_OPENGL_DEBUG_CONTEXT, c.GL_TRUE);
+
+        self.window = c.glfwCreateWindow(@intCast(c_int, width), @intCast(c_int, height), "Zigl", null, null);
+        if (self.window == null) {
+            panic("Failed to create GLFW window\n", .{});
+        }
+
+        c.glfwMakeContextCurrent(self.window);
+
+        if (c.gladLoadGLLoader(@ptrCast(c.GLADloadproc, c.glfwGetProcAddress)) == 0) {
+            panic("Failed to initialize GLAD\n", .{});
+        }
+
+        std.log.debug("GL_VENDOR: {s}", .{c.glGetString(c.GL_VENDOR)});
+        std.log.debug("GL_VERSION: {s}", .{c.glGetString(c.GL_VERSION)});
+        std.log.debug("GL_RENDERER: {s}", .{c.glGetString(c.GL_RENDERER)});
+
+        // Check for debug context
+        var flags: i32 = 0;
+        c.glGetIntegerv(c.GL_CONTEXT_FLAGS, &flags);
+        if (flags & c.GL_CONTEXT_FLAG_DEBUG_BIT != 0) {
+            std.log.debug("Debug context available", .{});
+        }
+
+        c.glDebugMessageCallback(opengl_debug_callback, null);
+        c.glEnable(c.GL_DEBUG_OUTPUT_SYNCHRONOUS);
+
+        try self.scene.init(global_allocator, width, height);
+
+        self.current_time = c.glfwGetTime();
+        self.last_time = self.current_time;
+        self.delta_time = 0.0;
+    }
+
+    pub fn run(self: *Self) void {
+        while (c.glfwWindowShouldClose(self.window) == 0) {
+            self.processInput();
+
+            self.scene.update(self.delta_time);
+
+            // Clear color and depth
+            const color = [_]c.GLfloat{ 0.1, 0.1, 0.1, 1.0 };
+            const depth = [_]c.GLfloat{0.0};
+            c.glClearBufferfv(c.GL_COLOR, 0, @ptrCast([*c]const c.GLfloat, &color));
+            c.glClearBufferfi(c.GL_DEPTH_STENCIL, 0, 1.0, 0);
+
+            self.scene.render();
+
+            c.glfwSwapBuffers(self.window);
+            c.glfwPollEvents();
+
+            self.tick();
+        }
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.scene.deinit();
+
+        std.log.debug("Deinitializing app", .{});
+        c.glfwDestroyWindow(self.window);
+        c.glfwTerminate();
+
+        const memory_leaked = gpa.deinit();
+
+        if (memory_leaked) {
+            std.log.debug("Memory leaked", .{});
+        }
+    }
+
+    fn tick(self: *Self) void {
+        self.current_time = c.glfwGetTime();
+        self.delta_time = @floatCast(f32, self.current_time - self.last_time);
+        self.last_time = self.current_time;
+    }
+
+    fn processInput(self: *Self) void {
+        if (c.glfwGetKey(self.window, c.GLFW_KEY_W) == c.GLFW_PRESS) {
+            self.scene.updateCamera(CameraMovement.forward, self.delta_time);
+        }
+
+        if (c.glfwGetKey(self.window, c.GLFW_KEY_S) == c.GLFW_PRESS) {
+            self.scene.updateCamera(CameraMovement.backward, self.delta_time);
+        }
+
+        if (c.glfwGetKey(self.window, c.GLFW_KEY_A) == c.GLFW_PRESS) {
+            self.scene.updateCamera(CameraMovement.left, self.delta_time);
+        }
+
+        if (c.glfwGetKey(self.window, c.GLFW_KEY_D) == c.GLFW_PRESS) {
+            self.scene.updateCamera(CameraMovement.right, self.delta_time);
+        }
+
+        if (c.glfwGetKey(self.window, c.GLFW_KEY_Q) == c.GLFW_PRESS) {
+            self.scene.updateCamera(CameraMovement.up, self.delta_time);
+        }
+
+        if (c.glfwGetKey(self.window, c.GLFW_KEY_E) == c.GLFW_PRESS) {
+            self.scene.updateCamera(CameraMovement.down, self.delta_time);
+        }
+    }
+};
+
+const GLDebugSource = enum(u32) {
+    api = 0x8246,
+    window_system = 0x8247,
+    shader_compiler = 0x8248,
+    third_party = 0x8249,
+    application = 0x824a,
+    other = 0x824b,
+};
+
+const GLDebugType = enum(u32) {
+    @"error" = 0x824c,
+    deprecated_behavior = 0x824d,
+    undefined_behavior = 0x824e,
+    portability = 0x824f,
+    performance = 0x8250,
+    other = 0x8251,
+    // Not sure about these 3
+    marker = 0x8268,
+    push_group = 0x8269,
+    pop_group = 0x826a,
+};
+
+const GLDebugSeverity = enum(u32) {
+    high = 0x9146,
+    medium = 0x9147,
+    low = 0x9148,
+    notification = 0x826b,
+};
+
+fn opengl_debug_callback(source: c.GLenum, message_type: c.GLenum, id: c.GLuint, severity: c.GLenum, length: c.GLsizei, message: [*c]const c.GLchar, userParam: ?*const c.GLvoid) callconv(.C) void {
+    const debug_source = @intToEnum(GLDebugSource, source);
+    const debug_type = @intToEnum(GLDebugType, message_type);
+    const debug_severity = @intToEnum(GLDebugSeverity, severity);
+
+    std.log.debug("[{}][{}][{}] - {s}", .{ debug_severity, debug_type, debug_source, message });
+
+    if (debug_severity == GLDebugSeverity.high) {
+        panic("An High severity message was received.", .{});
+    }
+
+    if (debug_type == GLDebugType.@"error") {
+        panic("An OpenGL error occurred.", .{});
+    }
+}
--- src/scene_renderer.zig
+++ src/scene_renderer.zig
@@ -32,7 +32,8 @@ pub const Node = struct {
     transform: ModelTransform,
 };
 
-pub const Scene = struct {
+pub const SceneRenderer = struct {
+    const Self = @This();
     global_params: GlobalParams,
 
     vao: c.GLuint,
@@ -55,29 +56,29 @@ pub const Scene = struct {
     allocator: *std.mem.Allocator,
 
     // TODO: Pass the path to a scene file
-    pub fn init(scene: *Scene, allocator: *std.mem.Allocator, screen_width: u32, screen_height: u32) !void {
-        scene.allocator = allocator;
+    pub fn init(self: *Self, allocator: *std.mem.Allocator, screen_width: u32, screen_height: u32) !void {
+        self.allocator = allocator;
         // Scene data container
         // --------------------
-        scene.camera.init(Vec3f.init(0, 0, 0), Vec3f.init(0, 1, 0), -90.0, 0.0);
+        self.camera.init(Vec3f.init(0, 0, 0), Vec3f.init(0, 1, 0), -90.0, 0.0);
 
         // Load all meshes
         // ---------------
         const mesh_count = 2; // TODO: This will be read for the scene file
-        scene.meshes = try allocator.alloc(model.Mesh, mesh_count);
+        self.meshes = try allocator.alloc(model.Mesh, mesh_count);
         var geometry: model.Geometry = undefined;
         geometry.init(allocator);
         // TODO: We will iterate over all the meses in the scene file
         {
-            scene.meshes[0] = try geometry.loadObj("data/models/default_cube.obj");
-            scene.meshes[1] = try geometry.loadObj("data/models/suzanne.obj");
+            self.meshes[0] = try geometry.loadObj("data/models/default_cube.obj");
+            self.meshes[1] = try geometry.loadObj("data/models/suzanne.obj");
         }
 
         // Load Textures
         // -------------
         const texture_count = 1; // TODO: This will be read from the scene file
-        scene.textures = try allocator.alloc(c.GLuint, texture_count);
-        c.glCreateTextures(c.GL_TEXTURE_2D, texture_count, @ptrCast([*c]c_uint, scene.textures.ptr));
+        self.textures = try allocator.alloc(c.GLuint, texture_count);
+        c.glCreateTextures(c.GL_TEXTURE_2D, texture_count, @ptrCast([*c]c_uint, self.textures.ptr));
 
         // TODO: We will iterate over all the textures in the scene file
         {
@@ -86,8 +87,8 @@ pub const Scene = struct {
             file.close();
             var pi = try PngImage.create(data);
 
-            c.glTextureStorage2D(scene.textures[0], 1, c.GL_RGBA8, @intCast(c_int, pi.width), @intCast(c_int, pi.height));
-            c.glTextureSubImage2D(scene.textures[0], 0, 0, 0, @intCast(c_int, pi.width), @intCast(c_int, pi.height), c.GL_RGBA, c.GL_UNSIGNED_BYTE, @ptrCast(*c_void, &pi.raw[0]));
+            c.glTextureStorage2D(self.textures[0], 1, c.GL_RGBA8, @intCast(c_int, pi.width), @intCast(c_int, pi.height));
+            c.glTextureSubImage2D(self.textures[0], 0, 0, 0, @intCast(c_int, pi.width), @intCast(c_int, pi.height), c.GL_RGBA, c.GL_UNSIGNED_BYTE, @ptrCast(*c_void, &pi.raw[0]));
 
             allocator.free(data);
             PngImage.destroy(&pi);
@@ -96,13 +97,13 @@ pub const Scene = struct {
         // Load Materials
         // --------------
         const material_count = 2; // TODO: This will be read for a scene file
-        scene.materials = try allocator.alloc(Material, material_count);
+        self.materials = try allocator.alloc(Material, material_count);
         // TODO: We will iterate over all the materials in the scene file
         {
-            scene.materials[0] = Material{
+            self.materials[0] = Material{
                 .albedo_texture_idx = 0,
             };
-            scene.materials[1] = Material{
+            self.materials[1] = Material{
                 .albedo_texture_idx = 0,
             };
         }
@@ -110,10 +111,10 @@ pub const Scene = struct {
         // Load Nodes
         // ----------
         const node_count = 2; // TODO: This will be read for a scene file
-        scene.nodes = try allocator.alloc(Node, node_count);
+        self.nodes = try allocator.alloc(Node, node_count);
         // TODO: We will iterate over all the nodesl in the scene file
         {
-            scene.nodes[0] = Node{
+            self.nodes[0] = Node{
                 .mesh_idx = 0,
                 .material_idx = 0,
                 .position = Vec3f.init(1.5, 0, -4),
@@ -121,11 +122,11 @@ pub const Scene = struct {
                 .scale = Vec3f.init(1, 1, 1),
                 .transform = undefined,
             };
-            scene.nodes[0].transform = ModelTransform{
-                .model_matrix = Mat4f.TRS(scene.nodes[0].position, scene.nodes[0].rotation, scene.nodes[0].scale),
+            self.nodes[0].transform = ModelTransform{
+                .model_matrix = Mat4f.TRS(self.nodes[0].position, self.nodes[0].rotation, self.nodes[0].scale),
             };
 
-            scene.nodes[1] = Node{
+            self.nodes[1] = Node{
                 .mesh_idx = 1,
                 .material_idx = 1,
                 .position = Vec3f.init(-1.5, 0, -4),
@@ -133,85 +134,86 @@ pub const Scene = struct {
                 .scale = Vec3f.init(1, 1, 1),
                 .transform = undefined,
             };
-            scene.nodes[1].transform = ModelTransform{
-                .model_matrix = Mat4f.TRS(scene.nodes[1].position, scene.nodes[1].rotation, scene.nodes[1].scale),
+            self.nodes[1].transform = ModelTransform{
+                .model_matrix = Mat4f.TRS(self.nodes[1].position, self.nodes[1].rotation, self.nodes[1].scale),
             };
         }
 
         // Load scene settings
-        scene.global_params = GlobalParams{
-            .view_matrix = scene.camera.getViewMatrix(),
+        self.global_params = GlobalParams{
+            .view_matrix = self.camera.getViewMatrix(),
             .proj_matrix = Mat4f.perspective(60.0, @intToFloat(f32, screen_width) / @intToFloat(f32, screen_height), 0.001, 1000.0),
         };
 
         // Create the Vertex Array Object
-        c.glGenVertexArrays(1, &scene.vao);
-        c.glBindVertexArray(scene.vao);
+        c.glGenVertexArrays(1, &self.vao);
+        c.glBindVertexArray(self.vao);
 
         // Upload all geometry to a vertex and index buffer on the GPU
         {
             // Allocate and initialize a vertex buffer object
-            c.glCreateBuffers(1, &scene.vbo);
-            c.glNamedBufferStorage(scene.vbo, @intCast(c_longlong, @sizeOf(model.Vertex) * geometry.vertices.items.len), geometry.vertices.items.ptr, 0);
+            c.glCreateBuffers(1, &self.vbo);
+            c.glNamedBufferStorage(self.vbo, @intCast(c_longlong, @sizeOf(model.Vertex) * geometry.vertices.items.len), geometry.vertices.items.ptr, 0);
 
             // Allocate and initialize an index buffer object
-            c.glCreateBuffers(1, &scene.ebo);
-            c.glNamedBufferStorage(scene.ebo, @intCast(c_longlong, @sizeOf(u32) * geometry.indices.items.len), geometry.indices.items.ptr, 0);
+            c.glCreateBuffers(1, &self.ebo);
+            c.glNamedBufferStorage(self.ebo, @intCast(c_longlong, @sizeOf(u32) * geometry.indices.items.len), geometry.indices.items.ptr, 0);
 
             // We no longer need a copy of the data on the CPU
             geometry.deinit();
         }
 
         // Bind the buffer to the vertex array object
-        c.glVertexArrayVertexBuffer(scene.vao, 0, scene.vbo, 0, @sizeOf(model.Vertex));
+        c.glVertexArrayVertexBuffer(self.vao, 0, self.vbo, 0, @sizeOf(model.Vertex));
 
         // Set up two vertex attributes.
         // Position
-        c.glVertexArrayAttribBinding(scene.vao, 0, 0);
-        c.glVertexArrayAttribFormat(scene.vao, 0, 3, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "position"));
+        c.glVertexArrayAttribBinding(self.vao, 0, 0);
+        c.glVertexArrayAttribFormat(self.vao, 0, 3, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "position"));
         c.glEnableVertexAttribArray(0);
         // Normal
-        c.glVertexArrayAttribBinding(scene.vao, 1, 0);
-        c.glVertexArrayAttribFormat(scene.vao, 1, 3, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "normal"));
+        c.glVertexArrayAttribBinding(self.vao, 1, 0);
+        c.glVertexArrayAttribFormat(self.vao, 1, 3, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "normal"));
         c.glEnableVertexAttribArray(1);
         // UV
-        c.glVertexArrayAttribBinding(scene.vao, 2, 0);
-        c.glVertexArrayAttribFormat(scene.vao, 2, 2, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "uv0"));
+        c.glVertexArrayAttribBinding(self.vao, 2, 0);
+        c.glVertexArrayAttribFormat(self.vao, 2, 2, c.GL_FLOAT, c.GL_FALSE, @byteOffsetOf(model.Vertex, "uv0"));
         c.glEnableVertexAttribArray(2);
 
         c.glBindVertexArray(0);
 
-        scene.uber_shader = try shaders.createProgram(allocator, "data/shaders/uber_vertex.glsl", "data/shaders/uber_fragment.glsl");
+        self.uber_shader = try shaders.createProgram(allocator, "data/shaders/uber_vertex.glsl", "data/shaders/uber_fragment.glsl");
 
         // Create samplers for the program
-        c.glCreateSamplers(1, &scene.albedo_sampler);
-        c.glSamplerParameteri(scene.albedo_sampler, c.GL_TEXTURE_WRAP_S, c.GL_CLAMP_TO_EDGE);
-        c.glSamplerParameteri(scene.albedo_sampler, c.GL_TEXTURE_WRAP_T, c.GL_CLAMP_TO_EDGE);
-        c.glSamplerParameteri(scene.albedo_sampler, c.GL_TEXTURE_MIN_FILTER, c.GL_LINEAR_MIPMAP_LINEAR);
-        c.glSamplerParameteri(scene.albedo_sampler, c.GL_TEXTURE_MAG_FILTER, c.GL_LINEAR);
+        c.glCreateSamplers(1, &self.albedo_sampler);
+        c.glSamplerParameteri(self.albedo_sampler, c.GL_TEXTURE_WRAP_S, c.GL_CLAMP_TO_EDGE);
+        c.glSamplerParameteri(self.albedo_sampler, c.GL_TEXTURE_WRAP_T, c.GL_CLAMP_TO_EDGE);
+        c.glSamplerParameteri(self.albedo_sampler, c.GL_TEXTURE_MIN_FILTER, c.GL_LINEAR_MIPMAP_LINEAR);
+        c.glSamplerParameteri(self.albedo_sampler, c.GL_TEXTURE_MAG_FILTER, c.GL_LINEAR);
 
-        c.glCreateBuffers(1, &scene.global_uniform_buffer);
-        c.glNamedBufferStorage(scene.global_uniform_buffer, @intCast(c_longlong, @sizeOf(GlobalParams)), &scene.global_params, c.GL_DYNAMIC_STORAGE_BIT);
+        c.glCreateBuffers(1, &self.global_uniform_buffer);
+        c.glNamedBufferStorage(self.global_uniform_buffer, @intCast(c_longlong, @sizeOf(GlobalParams)), &self.global_params, c.GL_DYNAMIC_STORAGE_BIT);
 
-        c.glCreateBuffers(1, &scene.node_uniform_buffer);
-        c.glNamedBufferStorage(scene.node_uniform_buffer, @intCast(c_longlong, @sizeOf(ModelTransform)), null, c.GL_DYNAMIC_STORAGE_BIT);
+        c.glCreateBuffers(1, &self.node_uniform_buffer);
+        c.glNamedBufferStorage(self.node_uniform_buffer, @intCast(c_longlong, @sizeOf(ModelTransform)), null, c.GL_DYNAMIC_STORAGE_BIT);
     }
 
-    pub fn deinit(self: *Scene) void {
+    pub fn deinit(self: *Self) void {
+        std.log.debug("Deinitializing scene renderer", .{});
         self.allocator.free(self.meshes);
         self.allocator.free(self.textures);
         self.allocator.free(self.materials);
         self.allocator.free(self.nodes);
     }
 
-    pub fn update(self: *Scene, delta_time: f32) void {
+    pub fn update(self: *Self, delta_time: f32) void {
     }
 
-    pub fn updateCamera(self: *Scene, direction: CameraMovement, delta_time: f32) void {
+    pub fn updateCamera(self: *Self, direction: CameraMovement, delta_time: f32) void {
         self.camera.processMovement(direction, delta_time);
     }
 
-    pub fn render(self: *Scene) void {
+    pub fn render(self: *Self) void {
         c.glBindVertexArray(self.vao);
 
         c.glUseProgram(self.uber_shader);
